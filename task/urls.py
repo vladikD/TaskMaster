d8f9b393905249b5042e8dd73d1466fc2cc5e092
vladikD <93707032+vladikD@users.noreply.github.com>
2025-03-29 18:16:48 +0200

    added a check for token validity

--- task/urls.py
+++ task/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet, LabelViewSet, ProjectViewSet, CommentViewSet, RegisterView, ObtainTokenView
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet)
 router.register(r'labels', LabelViewSet)
@@ -13,4 +17,8 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', ObtainTokenView.as_view(), name='login'),
     path('auth/', include('social_django.urls', namespace='social')),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
