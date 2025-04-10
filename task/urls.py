6f9533506643d8d02fcf94a4d7aded243dff38c9
vladikD <93707032+vladikD@users.noreply.github.com>
2025-04-10 22:30:36 +0300

    updated urls.py

--- task/urls.py
+++ task/urls.py
@@ -15,6 +15,20 @@ router.register(r'projects', ProjectViewSet)
 router.register(r'comments', CommentViewSet)
 router.register(r'columns', ColumnViewSet)
 
+project_add_user = ProjectViewSet.as_view({
+    'post': 'add_user'
+})
+project_remove_user = ProjectViewSet.as_view({
+    'delete': 'remove_user'
+})
+
+task_assign_user = TaskViewSet.as_view({
+    'patch': 'assign_user'
+})
+task_unassign_user = TaskViewSet.as_view({
+    'delete': 'unassign'
+})
+
 urlpatterns = [
     path('api/', include(router.urls)),
     path('register/', RegisterView.as_view(), name='register'),
@@ -23,6 +37,12 @@ urlpatterns = [
 
     path('invitations/create/', InvitationCreateView.as_view(), name='invitation-create'),
     path('invitations/accept/', InvitationAcceptView.as_view(), name='invitation-accept'),
+
+    path('api/projects/<int:pk>/add-user/', project_add_user, name='project-add-user'),
+    path('api/projects/<int:pk>/remove-user/<int:user_id>/', project_remove_user, name='project-remove-user'),
+
+    path('api/tasks/<int:pk>/assign/', task_assign_user, name='task-assign-user'),
+    path('api/tasks/<int:pk>/unassign/', task_unassign_user, name='task-unassign-user'),
 
     path('project/<int:pk>/full/', ProjectDetailNestedView.as_view(), name='project-detail-nested'),
 
