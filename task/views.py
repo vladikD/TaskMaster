ed12679e30b81adbf4d7ecbac895923ef7db02fa
vladikD <93707032+vladikD@users.noreply.github.com>
2025-04-21 15:04:09 +0300

    removed an outdated comment

--- task/views.py
+++ task/views.py
@@ -24,21 +24,6 @@ from django.utils import timezone
 from django.shortcuts import redirect
 import datetime
 
-
-
-# Creating a register view
-# class RegisterView(APIView):
-#     permission_classes = [AllowAny]
-#
-#     def post(self, request):
-#         serializer = UserSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response({'message': 'User created successfully!'}, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# task/views.py
-
 class RegisterView(APIView):
     permission_classes = [AllowAny]
 
